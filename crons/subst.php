14023e521f2ba5dffa59398159ec56cf4af48a57
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-14 20:03:54 +0200

    new stuff

--- crons/subst.php
+++ crons/subst.php
@@ -40,9 +40,28 @@ if (!$conn) {
 	exit();
 }
 
+/*
 if(!mysqli_select_db("akgwebservices", $conn)) {
 	printf("Selection failed: %s\n", mysqli_error($conn));
 	exit();
+}
+*/
+
+/* return name of current default database */
+if ($result = mysqli_query($conn, "SELECT DATABASE()")) {
+    $row = mysqli_fetch_row($result);
+    printf("Default database is %s.\n", $row[0]);
+    mysqli_free_result($result);
+}
+
+/* change db to world db */
+mysqli_select_db($conn, "akgwebservices");
+
+/* return name of current default database */
+if ($result = mysqli_query($conn, "SELECT DATABASE()")) {
+    $row = mysqli_fetch_row($result);
+    printf("Default database is %s.\n", $row[0]);
+    mysqli_free_result($result);
 }
 
 if (!mysqli_query($conn, SQL_CREATE) || !mysqli_query($conn, SQL_CLEAR)) {
