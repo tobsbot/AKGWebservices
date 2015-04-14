8c4fe6f4d612b88e6c46fc56d992fb21b161921b
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-14 14:22:27 +0200

    new stuff

--- crons/subst.php
+++ crons/subst.php
@@ -5,7 +5,7 @@ include('lib/simple_html_dom.php');
 define("URL_SUBST", "http://www.akg-bensheim.de/akgweb2011/content/Vertretung/w/%02d/w00000.htm");
 define("SEL_SUBST", "#vertretung table.subst tr[class=list odd], #vertretung table.subst tr[class=list even]");
 
-$credentials = json_decode('lib/credentials.json') ->akgwebservices;
+$credentials = json_decode('lib/credentials.json', true)['akgwebservices'];
 $log = new Log('log/subst.txt');
 
 echo("Password: " . $credentials->pair->passwd);
