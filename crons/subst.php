11c237a4b9ede87b620c1f216052832d9468f490
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-14 14:57:07 +0200

    new stuff

--- crons/subst.php
+++ crons/subst.php
@@ -6,7 +6,6 @@ include('lib/simple_html_dom.php');
 define("URL_SUBST", "http://www.akg-bensheim.de/akgweb2011/content/Vertretung/w/%02d/w00000.htm");
 define("SEL_SUBST", "#vertretung table.subst tr[class=list odd], #vertretung table.subst tr[class=list even]");
 
-echo 'Fuck you';
-$credentials = json_decode(file_get_contents('lib/credentials.json')) ->akgwebservices;
-var_dump($credentials);
+$credentials = Json::decode(file_get_contents('lib/credentials.json')) ->akgwebservices ->pair;
+$log = new Log('log/subst.txt');
 ?>
