438f5125ff205974f17518b839bb1fff073ae7b0
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-14 14:40:42 +0200

    new stuff

--- crons/subst.php
+++ crons/subst.php
@@ -1,11 +1,12 @@
 <?php
 include('lib/log.php');
+include('lib/json.php');
 include('lib/simple_html_dom.php');
 
 define("URL_SUBST", "http://www.akg-bensheim.de/akgweb2011/content/Vertretung/w/%02d/w00000.htm");
 define("SEL_SUBST", "#vertretung table.subst tr[class=list odd], #vertretung table.subst tr[class=list even]");
 
-$credentials = json_decode('lib/credentials.json', true);
+$credentials = Json::decode('lib/credentials.json');
 
 var_dump($credentials);
 ?>
