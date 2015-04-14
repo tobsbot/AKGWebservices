aee3a11bbe6d321a6e17aaccfcfa4287e7ecc80a
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-14 14:11:34 +0200

    new stuff

---
+++ crons/lib/log.php
@@ -0,0 +1,30 @@
+<?php
+	class Log {
+		public $file;
+
+		public function __construct($file) {
+			$this->file = $file;
+		}
+
+		public function i($str) {
+			$this->log("INFO", $str);
+		}
+
+		public function d($str) {
+			$this->log("DEBUG", $str);
+		}
+
+		public function w($str) {
+			$this->log("WARNING", $str);
+		}
+
+		public function e($str) {
+			$this->log("ERROR", $str);
+		}
+
+		public function log($level, $str) {
+			$timestamp = date("d.m-Y H:i:s", time());
+			file_put_contents($this->file, "[$timestamp] ($level) $str", FILE_APPEND | LOCK_EX);
+		}
+	}
+?>
---
+++ crons/subst.php
@@ -0,0 +1,12 @@
+<?php
+include('lib/log.php');
+include('lib/simple_html_dom.php');
+
+define("URL_SUBST", "http://www.akg-bensheim.de/akgweb2011/content/Vertretung/w/%02d/w00000.htm");
+define("SEL_SUBST", "#vertretung table.subst tr[class=list odd], #vertretung table.subst tr[class=list even]");
+
+$credentials = json_decode('lib/credentials.json') ->akgwebservices;
+$log = new Log('log/subst.txt');
+
+$log->i("Password: " . $credentials->pair->passwd);
+?>
