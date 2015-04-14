a816fe0eda80ff706b8697dac6d6796505726067
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-14 21:05:23 +0200

    new stuff

--- crons/subst.php
+++ crons/subst.php
@@ -98,6 +98,7 @@ mysqli_close($conn);
 function get_data($url) {
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_ENCODING ,"");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
 	$data = curl_exec($ch);
