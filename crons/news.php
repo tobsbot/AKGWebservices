77b0cec2c2d1a18d18b1340c04e6719399f06ed8
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-18 22:27:19 +0200

    minor

---
+++ crons/news.php
@@ -0,0 +1,85 @@
+<?php
+include('lib/utils.php');
+include('lib/simple_html_dom.php');
+
+header('Content-Type: text/plain; charset=utf-8');
+define("URL", "http://www.akg-bensheim.de/?start=%d");
+
+
+##########################################################
+print("Establishing connection to database...\r\n");
+#########################################################
+
+$database = json_decode(file_get_contents("lib/database.json"));
+$conn = mysqli_connect(
+	getenv($database ->server),
+	$database ->credentials ->user,
+	$database ->credentials ->passwd
+);
+
+if (!$conn) {
+	printf("Connect failed: %s\r\n", mysqli_connect_error());
+	exit();
+}
+
+##########################################################
+print("Connected to database. Ensuring datasource...\r\n");
+##########################################################
+
+if (!mysqli_select_db($conn, $database ->name)) {
+	printf("Selection failed: %s\r\n", mysqli_error($conn));
+	exit();
+}
+
+if (!mysqli_query($conn, $database ->tables ->News ->create)) {
+	printf("Creation / Clearing failed: %s\r\n", mysqli_error($conn));
+	exit();
+}
+
+$insert = mysqli_prepare(
+	$conn,
+	$database ->tables ->News ->insert
+);
+
+mysqli_stmt_bind_param(
+	$insert,
+	'ssss',
+	$title,
+	$article,
+	$imageUrl,
+	$imageDesc
+);
+
+$start = 0;
+$end = 0;
+do {
+	printf("\r\nParsing \"" . URL . "\" ...\r\n", $start);
+
+	$html = str_get_html(
+		get_data(
+			sprintf(URL, $start)
+		)
+	);
+
+	if(empty($html)) {
+		print("No resource on this url!\r\n");
+		continue;
+	}
+
+	$tmp = $html ->find('li.pagination-end a.pagenav', 0) ->href;
+	print($tmp . "\r\n");
+
+	$start += 4;
+} while ($start < $end);
+
+##########################################################
+print("\r\nClosing connection to database...\r\n");
+##########################################################
+
+mysqli_stmt_close($insert);
+mysqli_close($conn);
+
+##########################################################
+print("Cron job successfully finished!\r\n");
+##########################################################
+?>
