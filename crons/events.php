36950c54eee2c0aca21d28b97d36144f7de0fe45
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-18 17:23:30 +0200

    minor

--- crons/events.php
+++ crons/events.php
@@ -74,12 +74,22 @@ foreach($html ->find('#jevents_body table.ev_table tbody tr') as $tr) {
 	$date = parseEventDate($tr ->find('td.ev_td_left text', 0) ->plaintext);
 	foreach($tr ->find('li.ev_td_li') as $li) {
 		$a = $li ->find('a.ev_link_row', 0);
-		$url = $a ->href;
 
 		$title = tidyUp($a ->plaintext);
 		$dateString = tidyUp($li ->find('text', 0) ->plaintext);
 
-		print("Parsed: [$title, $date, $dateString]\n");
+		$htmlDatil = str_get_html(get_data($a ->href));
+		$description = "";
+		if(!empty($htmlDatil)) {
+
+			// Iterate through all layout rows that make up the description
+			foreach($htmlDatil ->find('#jevents_body table.contentpaneopen tr[!class]') as $tr_temp) {
+
+				// Add the description to the description string
+				$description .= tidyUp($tr_temp ->plaintext);
+	    	}
+		}
+		print("Parsed: [$title, $date, $dateString, $description]\n");
 	}
 }
 	/*
