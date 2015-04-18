99187a9b6e26698da6a73c8cd27c29aa38b38d09
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-18 19:35:13 +0200

    minor

---
+++ events.php
@@ -0,0 +1,41 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+if(!isset($_GET['start'])) {
+	json_response(NULL, 403, "No start value passed!");
+}
+
+$database = json_decode(file_get_contents("crons/lib/database.json"));
+$conn = mysqli_connect(
+	getenv($database ->server),
+	$database ->credentials ->user,
+	$database ->credentials ->passwd
+);
+
+if (!$conn) {
+	json_response(NULL, 500, "Connection to database failed!");
+}
+
+if (!mysqli_select_db($conn, $database ->name)) {
+	mysqli_close($conn);
+	json_response(NULL, 500, "Selection of datasource failed!");
+}
+
+$sth = mysqli_query($conn, "SELECT * FROM Events ORDER BY eventDate ASC LIMIT 10 OFFSET $_GET['start']");
+$rows = array();
+while($r = mysqli_fetch_assoc($sth)) {
+    $rows[] = $r;
+}
+
+mysqli_close($conn);
+json_response($rows);
+
+function json_response($data, $code = 200, $msg = "OK") {
+	print json_encode([
+		"code" => $code,
+		"message" => $msg,
+		"data" => $data
+	], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+	exit();
+}
+?>
