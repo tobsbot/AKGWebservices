493265270c059c5928fd7d11a80dea119c3ca54a
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-18 17:04:24 +0200

    minor

--- crons/events.php
+++ crons/events.php
@@ -66,7 +66,7 @@ if (empty($html)) {
 }
 
 foreach($html ->find('#jevents_body table.ev_table tbody tr') as $tr) {
-	if(empty($tr ->plaintext))
+	if(empty(trim($tr ->plaintext)))
 		continue;
 
 	$date = parseEventDate($tr ->find('td.ev_td_left text', 0) ->plaintext);
