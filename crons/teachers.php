d2530bb896598dc2ee49a49dbbea4294d59f3eea
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-18 17:55:48 +0200

    minor

---
+++ crons/teachers.php
@@ -0,0 +1,32 @@
+<?php
+include('lib/utils.php');
+include('lib/simple_html_dom.php');
+
+header('Content-Type: text/plain; charset=utf-8');
+
+##########################################################
+print("Establishing connection to database...\r\n");
+#########################################################
+
+$database = json_decode(file_get_contents("lib/database.json"));
+$conn = mysqli_connect(
+	getenv($database ->server),
+	$database ->credentials ->user,
+	$database ->credentials ->passwd
+);
+
+if (!$conn) {
+	printf("Connect failed: %s\r\n", mysqli_connect_error());
+	exit();
+}
+
+##########################################################
+print("Connected to database. Ensuring datasource...\r\n");
+##########################################################
+
+if (!mysqli_select_db($conn, $database ->name)) {
+	printf("Selection failed: %s\r\n", mysqli_error($conn));
+	exit();
+}
+
+?>
