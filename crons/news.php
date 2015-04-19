ce8987616d9de397cdb0e40ef8251f221c246001
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-04-19 13:37:55 +0200

    minor

--- crons/news.php
+++ crons/news.php
@@ -84,7 +84,7 @@ do {
 		$article = "";
 		if(!empty($tmpHtml)) {
 			$imageUrl = 'http://www.akg-bensheim.de' . $tmpHtml ->find('div.item-page img', 0) ->src;
-			$imageDesc = tidyUp($tmpHtml ->find('p.img_caption', 0) ->plaintext);
+			$imageDesc = "Hallo";//tidyUp($tmpHtml ->find('p.img_caption', 0) ->plaintext);
 
 			foreach($tmpHtml ->find('div.item-page p[!class], div.item-page div[!class]') as $p_tmp) {
 				$article .= tidyUp($p_tmp ->plaintext);
