cd2862fc100ba4d9e813273ad255a2159870e40b
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-05-03 19:19:46 +0200

    minor

--- thumb.php
+++ thumb.php
@@ -1,4 +1,4 @@
 <?php
 require_once('phpthumb/phpThumb.config.php');
-echo '<img src="'.htmlspecialchars(phpThumbURL('src=http://www.akg-bensheim.de/images/Mathematik/Kg2015_Preistrger_HP.JPG&w=320&q=50', 'phpthumb/phpThumb.php')).'">';
+readfile(phpThumbURL('src=http://www.akg-bensheim.de/images/Mathematik/Kg2015_Preistrger_HP.JPG&w=320&q=50', 'phpthumb/phpThumb.php'));
 ?>
