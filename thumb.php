1399934af32f1d899b8e457dddd1e7e5fa66d0dd
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-05-03 18:48:55 +0200

    minor

--- thumb.php
+++ thumb.php
@@ -1,4 +1,4 @@
 <?php
 require_once('phpthumb/phpThumb.config.php');
-readfile(phpThumbURL('src=http://www.akg-bensheim.de/images/Mathematik/Kg2015_Preistrger_HP.JPG&w=50', 'phpthumb/phpThumb.php'));
+echo phpThumbURL('src=http://www.akg-bensheim.de/images/Mathematik/Kg2015_Preistrger_HP.JPG&w=50', 'phpthumb/phpThumb.php');
 ?>
