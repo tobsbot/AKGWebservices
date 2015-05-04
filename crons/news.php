1334d57744740e95d2bfa34a6a67ff8e777f3498
P0T4T0x <P0T4T0x@users.noreply.github.com>
2015-05-05 00:36:28 +0200

    minor

--- crons/news.php
+++ crons/news.php
@@ -91,7 +91,7 @@ do {
 			$imageDesc = tidyUp($tmpHtml ->find('div.item-page img', 0) ->title);
 
 			foreach($tmpHtml ->find('div.item-page p[class!=img_caption], div.item-page p[!class], div.item-page div[!class]') as $p_tmp) {
-				if(!empty($article))
+				if(!empty($article) && !endsWith($article, "\r\n"))
 					$article .= "\r\n\r\n";
 
 				$article .= tidyUp($p_tmp ->plaintext);
