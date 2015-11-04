0f77ab355167bb82857cbec0521e370fc23c5032
Tobias Erthal <android@akgbensheim.de>
2015-11-04 23:58:21 +0100

    cron environment changes

--- .openshift/cron/minutely/subst.sh
+++ .openshift/cron/minutely/subst.sh
@@ -2,7 +2,7 @@
 if [ ! -f $OPENSHIFT_DATA_DIR/logs/subst ]; then
   touch $OPENSHIFT_DATA_DIR/logs/subst
 fi
-if [[ $(find $OPENSHIFT_DATA_DIR/last_run -mmin +4) ]]; then #run every 5 mins
+if [[ $(find $OPENSHIFT_DATA_DIR/logs/subst -mmin +4) ]]; then #run every 5 mins
   rm -f $OPENSHIFT_DATA_DIR/logs/subst
   touch $OPENSHIFT_DATA_DIR/logs/subst
 
